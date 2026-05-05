c1c616d7acac04652dbaee3868c1024baba7732d
Didrik <didrik.varma@gmail.com>
2026-05-05 16:17:18 +0200

    New submission for lab1

--- backend/test/unit/test_DAO.py
+++ backend/test/unit/test_DAO.py
@@ -1,100 +1,100 @@
 import pytest
 from pymongo import MongoClient
 from src.util.dao import DAO
+from pymongo.errors import WriteError
 
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
+
+
+@pytest.fixture
+def mocked_validator():
+    return {
+        "$jsonSchema": {
+            "bsonType": "object",
+            "required": ["name", "description"],
+            "properties": {
+                "name": {
+                    "bsonType": "string"
+                },
+                "description": {
+                    "bsonType": "string"
+                }
+            }
+        }
+    }
 
 
 @pytest.fixture
 def create_db():
     client = MongoClient("mongodb://localhost:27017/")
-    db = client["test"]
+    db = client.edutask
+    collection_name = "test_task_create"
+
+    db.drop_collection(collection_name)
 
     yield db
 
-    client.drop_database("test")
+    db.drop_collection(collection_name)
+    client.close()
 
 
-def test_create_valid(create_db):
+def test_create_valid_input(create_db, mocked_validator):
+    db = create_db
+    collection_name = "test_task_create"
+
+    with patch("src.util.dao.getValidator", return_value=mocked_validator):
+        dao = DAO(collection_name)
+    data = {
+        "name": "Test",
+        "description": "jewniuw"
+    }
+
+    result = dao.create(data)
+    assert result["name"] == "Test"
+    assert result["description"] == "jewniuw"
+
+    db_result = db[collection_name].find_one({"name": "Test"})
+    assert db_result is not None
+
+
+def test_validator_reject(create_db, mocked_validator):
+    db = create_db
+    collection_name = "test_task_create"
+
+    with patch("src.util.dao.getValidator", return_value=mocked_validator):
+        dao = DAO(collection_name)
+
+    invalid_data = {
+        "name": 123,
+        "description": "Not string"
+    }
+
+    with pytest.raises(WriteError):
+        dao.create(invalid_data)
+    db_result = db[collection_name].find_one({
+        "description": "Not string"
+    })
+    assert db_result is None
+
+
+def test_mongodb_fail():
     dao = DAO("task")
-
-    dao.db = create_db
-    dao.collection = create_db["task"]
+    dao.collection = MagicMock()
+    dao.collection.insert_one.side_effect = Exception()
 
     data = {
         "name": "Test",
-        "description": "lieuhipuegah"
+        "description": "kjwniprn"
     }
+    with pytest.raises(Exception):
+        dao.create(data)
 
-    dao.create(data)
 
-    result = create_db["task"].find_one({"name": "Test"})
-    assert result is not None
-    
-def test_create_data_not_dict(create_db):
+def test_create_input_not_dictionary():
     dao = DAO("task")
-    dao.db = create_db
-    dao.collection = create_db["task"]
-
+    dao.collection = MagicMock()
     data = 1
 
     with pytest.raises(Exception):
         dao.create(data)
-        
-def test_create_with_diffrent_datatypes_in_dict(create_db):
-    dao = DAO("task")
-    dao.db = create_db
-    dao.collection = create_db["task"]
-
-    data = {"invalid": "data", "name": "Test", "number": 123, "list": [1, 2, 3], "dict": {"key": "value"}}
-
-    dao.create(data)
-
-    result = create_db["task"].find_one({"name": "Test"})
-    assert result is not None
-    
-def test_database_invalid_values(create_db):
-    dao = DAO("task")
-    dao.collection = MagicMock()
-    data = 1
-    with pytest.raises(Exception):
-        dao.create(data)
-        
-def test_create_with_empty_dict():
-    dao = DAO("task")
-    dao.collection = MagicMock()
-    data = {}
-    with pytest.raises(Exception):
-        dao.create(data)
-    
-def test_create_duplicated_dict(create_db):
-    
-    dao = DAO("task")
-    dao.db = create_db
-    dao.collection = create_db["task"]
-    data = {
-        "name": "Test",
-        "description": "lieuhipuegah"
-    }
-    
-    data2 = {
-        "name": "Test",
-        "description": "iuheiuabe"
-    }
-
-    dao.create(data)
-    dao.create(data2)
-    
-
-    result = create_db["task"].find_one({"name": "Test"})
-    assert result is not None
-    
-def test_create_mongo_exception_error():
-    dao = DAO("task")
-    dao.collection = MagicMock()
-
-    dao.collection.insert_one.side_effect = Exception("Error")
-
-    with pytest.raises(Exception):
-        dao.create({"name": "Test"})
     
