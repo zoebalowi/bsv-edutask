eb67a5b2bda982f42b37a56fee85cf68fbc14c54
Didrik <didrik.varma@gmail.com>
2026-05-14 11:16:28 +0200

    Changed the get_user_by_email method to pass unittest

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -32,9 +32,11 @@ class UserController(Controller):
             users = self.dao.find({'email': email})
             if len(users) == 1:
                 return users[0]
-            else:
+            elif len(users) > 1:
                 print(f'Error: more than one user found with mail {email}')
                 return users[0]
+            else:
+                return None
         except Exception as e:
             raise
 
